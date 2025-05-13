60ab50f0a8a9ad56da1a5293c511cd61febac664
XuHaijwill <zuiwoxuanlou@163.com>
2025-05-14 00:02:29 +0800

    fix the issue, please also common the jdk version

--- java/org/apache/naming/StringManager.java
+++ java/org/apache/naming/StringManager.java
@@ -16,6 +16,7 @@
  */
 package org.apache.naming;
 
+import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
 import java.util.Hashtable;
 import java.util.Locale;
@@ -125,6 +126,15 @@ public class StringManager {
             //      simply return null.  Calling code can then do
             //      a null check.
             str = null;
+        }
+
+        // 日志打印时会读取系统默认的语言，将原本的 iso-8859-1 转换成 utf-8
+        if (str != null) {
+            try {
+                str = new String(str.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+            } catch (Exception e) {
+
+            }
         }
 
         return str;
